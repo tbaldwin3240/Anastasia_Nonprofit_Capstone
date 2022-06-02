5dea1d93584003c1b2c7f19a73cf02d24fc07820
KenasiaTurner <kenasi123@gmail.com>
2022-06-02 11:05:34 -0400

    adds the services section

---
+++ ui/website/src/components/Holder.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Holder.css';
+import HoverBox from './HoverBox';
+
+
+
+
+class Holder extends React.Component {
+    render(){
+        return (
+            <div style={{background:'green', width: '100vw', height: '100vh'}}>
+            <h1 style={{color:'white'}}> Holder</h1>
+        <div className='body'>
+            <div className='divflex'>
+
+            <HoverBox
+                boximage=''
+                boxlink=''
+                boxwords='super mario'
+            />
+
+                </div>
+            </div>
+        </div>
+        )
+    }
+}
+export default Holder;
